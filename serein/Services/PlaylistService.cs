8126a4c3badc321f896ace4faba3f40d57e7b65a
jyn626 <tuyor.jynn@gmail.com>
2026-07-27 13:32:14 +0800

    refactor: scan files only once instead of going through for every extentions

--- serein/Services/PlaylistService.cs
+++ serein/Services/PlaylistService.cs
@@ -5,27 +5,18 @@ namespace serein.Services
 {
     public class PlaylistService
     {
-
+        private static readonly string[] AllowedExtensions = { ".mp3", ".m4a" };
         public static List<Song> LoadSongs(string folder)
         {
-            // TODO: look for better ways to include two file types.
-            string[] m4aFiles= Directory.GetFiles(folder, "*.m4a");
-            string[] mp3Files= Directory.GetFiles(folder, "*.mp3");
-
-            // songs list will stores mp3 and m4a files
-            // a list of `Song` object.
             List<Song> songs = new();
 
-            // iterate over m4a and mp3s and add them each in the songs list
-            foreach (var mp3 in mp3Files) {
-                // ReadMetadata returns Song(model) object containing title, filepath, artist, album image?, and duration.
-                Song song = MetadataService.ReadMetadata(mp3);
-                songs.Add(song);
-            }
-            foreach (var m4a in m4aFiles)
-            {
-                Song song = MetadataService.ReadMetadata(m4a);
-                songs.Add(song);
+            foreach(string file in Directory.EnumerateFiles(folder)) {
+                string ext = Path.GetExtension(file);
+
+                if (AllowedExtensions.Contains(ext, StringComparer.OrdinalIgnoreCase))
+                 {
+                    songs.Add(MetadataService.ReadMetadata(file));
+                }
             }
 
             return songs;
